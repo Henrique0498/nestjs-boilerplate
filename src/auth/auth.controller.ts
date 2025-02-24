bb72eae0ec2e5b84ce26cb5a30f483e8cd8ca325
Henrique Lopes <h.lopes.silva2015@gmail.com>
2025-02-24 00:50:52 -0300

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+
+@Controller('v1/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  create(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.create(createAuthDto);
+  }
+}
---
+++ src/services/keys.ts
@@ -0,0 +1,2 @@
+export const IS_PRIVATE_KEY = process.env.SECRET_KEY
+export const IS_PUBLIC_KEY = process.env.PUBLIC_KEY
